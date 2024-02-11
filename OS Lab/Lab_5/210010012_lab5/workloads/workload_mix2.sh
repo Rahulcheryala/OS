c1207163e710e2ff6b8b243ccecd458beaaf4fbe
Rahulcheryala <rahulcheryala2004@gmail.com>
2024-02-12 00:24:00 +0530

    lab5

---
+++ OS Lab/Lab_5/210010012_lab5/workloads/workload_mix2.sh
@@ -0,0 +1,7 @@
+!/bin/sh
+./arithoh.sh &
+./arithoh.sh &
+./arithoh.sh &
+./fstime.sh &
+./syscall.sh &
+wait
---
+++ OS Lab/Lab_5/210010012_lab5/workloads/workload_mix4.sh
@@ -0,0 +1,7 @@
+!/bin/sh
+./fstime.sh &
+./fstime.sh &
+./syscall.sh &
+./syscall.sh &
+./syscall.sh &
+wait
